acef08a7360d7cc914973f04eaabf4d7c10b2fd3
Nob Yuasys <yuasys@gmail.com>
2022-12-13 09:16:40 +0900

    Update index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold text-center">
       Hello world!
     </h1>
   )
