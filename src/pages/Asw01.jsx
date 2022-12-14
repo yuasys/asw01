6218ca9a0e16c336368c5c6dad4469f44c6e1f16
Nob Yuasys <yuasys@gmail.com>
2022-12-15 02:49:47 +0900

    Add new contents

--- src/pages/Asw01.jsx
+++ src/pages/Asw01.jsx
@@ -1,22 +1,34 @@
 const Asw01 = () => {
   return (
     <>
-      <h1 className="my-4 mx-auto text-center bg-green-100 text-2xl">
-        ASW01_背景画像の実験1_幅変化に対応し背景も追従
+      <h1 className="mx-auto py-1 text-center bg-green-100 text-xl">
+        ASW01_背景画像をどこから取得するかの実験_01<br/>
+        <span className="text-lg">画像ファイルは/public直下に配置</span>
       </h1>
       <section> 
         <div>
-          <div className="w-full bg-contain xl:bg-cover h-screen bg-no-repeat"  style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asw_01_top.jpg'})`}}>
+          <div className="w-full xl:bg-cover bg-contain h-screen bg-no-repeat"  style={{ backgroundImage: `url(${process.env.PUBLIC_URL + '/asw_01_top.jpg'})`}}>
             <p className="text-white text-3xl p-12">白い文字をのせました</p>
-            <div className=" bg-black rounded-xl opacity-50 w-6/12 mx-auto p-3">
+            <div className=" bg-black rounded-xl opacity-50 w-96 mx-auto p-3">
               <p className="text-red-300 opacity-100 ">className="w-full bg-contain xl:bg-cover h-screen bg-no-repeat"<br/>
                   style={'{{ backgroundImage: `url(${process.env.PUBLIC_URL + '}{"'/asw_01_top.jpg'})`}}"}
               </p>
             </div>
           </div>
-    
-        </div>
+          <div className="mx-auto py-1 text-center bg-green-100 text-xl">
+              ASW01_背景画像をどこから取得するかの実験_02<br/>
+              <span className="text-lg">画像ファイルは外部Wordpressにある</span>
+            </div>
+            {/* https://dev-01.adsignweb.com/wp-content/uploads/2022/12/asw_01_mission.jpg */}
+            <div className="w-full xl:bg-cover bg-contain h-screen bg-no-repeat"  style={{ backgroundImage: `url("https://dev-01.adsignweb.com/wp-content/uploads/2022/12/asw_01_mission.jpg")`}}>
+
+            </div>
+            
+        </div> 
       </section>
+
+      <section>
+     </section>
     </>
   );
 };
